fb6ebb9b21fb86f4dada78b2a230787057162320
zmgu <122958434+zmgu@users.noreply.github.com>
2023-11-23 00:10:55 +0900

    Initial commit

---
+++ src/main/java/com/okhwa/okdol/OkdolApplication.java
@@ -0,0 +1,13 @@
+package com.okhwa.okdol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkdolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkdolApplication.class, args);
+	}
+
+}
