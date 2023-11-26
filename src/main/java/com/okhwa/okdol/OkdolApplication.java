1bcebd16d5450c4902b5a6ecb5cfc392a7b31477
zmgu <122958434+zmgu@users.noreply.github.com>
2023-11-26 23:26:23 +0900

    SpringApplicationBuilder

--- src/main/java/com/okhwa/okdol/OkdolApplication.java
+++ src/main/java/com/okhwa/okdol/OkdolApplication.java
@@ -2,10 +2,16 @@ package com.okhwa.okdol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OkdolApplication {
+public class OkdolApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OkdolApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OkdolApplication.class, args);
 	}
