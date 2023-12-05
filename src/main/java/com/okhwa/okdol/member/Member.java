1309e110b09f684d26ebc80a8b68a4eec4be8f3f
zmgu <122958434+zmgu@users.noreply.github.com>
2023-12-05 17:56:56 +0900

    member Entity 생성

---
+++ src/main/java/com/okhwa/okdol/member/Member.java
@@ -0,0 +1,31 @@
+package com.okhwa.okdol.member;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private Date birth;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+}
