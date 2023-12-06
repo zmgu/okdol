57e45787e9b7c3770b7594654767dea2d29a1c45
zmgu <122958434+zmgu@users.noreply.github.com>
2023-12-06 21:13:14 +0900

    함수명 변경

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -22,7 +22,7 @@ function declaration(tag) {
 }
 
 // 실패 함수
-function fail(tag, message) {
+function failTag(tag, message) {
 
     let elements = declaration(tag);
 
@@ -34,7 +34,7 @@ function fail(tag, message) {
 }
 
 // 성공 함수
-function success(tag) {
+function successTag(tag) {
 
     let elements = declaration(tag);
     
@@ -46,13 +46,26 @@ function success(tag) {
 document.getElementById('id').addEventListener('blur', function () {
 
     if  (!this.value) {
-        fail('id' ,'이메일을 입력해 주세요.');
+        failTag('id' ,'이메일을 입력해 주세요.');
 
     } else if (!emailPattern.test(this.value)) {
-        fail('id', '이메일 형식에 맞지 않습니다.');
+        failTag('id', '이메일 형식에 맞지 않습니다.');
 
     } else {
-        success('id');
+
+        $.ajax({
+            url: '/api/members/checkEmail/' + email,
+            type: 'GET',
+            success: function(response) {
+                // 서버로부터의 응답을 처리
+                document.getElementById('emailStatus').innerText = response;
+                successTag('id');
+
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     }
 });
 
@@ -60,10 +73,10 @@ document.getElementById('id').addEventListener('blur', function () {
 document.getElementById('password').addEventListener('input', function () {
 
     if (!passwordPattern.test(this.value)) {
-        fail('password', '비밀번호는 8~16자의 숫자, 영문, 특수문자를 조합하여 사용해 주세요.')
+        failTag('password', '비밀번호는 8~16자의 숫자, 영문, 특수문자를 조합하여 사용해 주세요.')
 
     } else {
-        success('password');
+        successTag('password');
         validatePasswordConfirm();
     }
 });
@@ -71,7 +84,7 @@ document.getElementById('password').addEventListener('input', function () {
 document.getElementById('password').addEventListener('blur', function () {
 
     if (!this.value) {
-        fail('password', '비밀번호를 입력해 주세요.');
+        failTag('password', '비밀번호를 입력해 주세요.');
     }
 });
 
@@ -83,21 +96,21 @@ function validatePasswordConfirm() {
         passwordConfirm = declaration('passwordConfirm');
 
     if(password.value == passwordConfirm[0].value && passwordPattern.test(passwordConfirm[0].value)) {
-        success('passwordConfirm');
+        successTag('passwordConfirm');
 
     } else if(!passwordConfirm[0].value) { 
         passwordConfirm[1].style.display = 'none';
         passwordConfirm[1].classList.add('is-invalid');
 
     } else {
-        fail('passwordConfirm', '비밀번호가 일치하지 않습니다.');
+        failTag('passwordConfirm', '비밀번호가 일치하지 않습니다.');
     }
 }
 
 document.getElementById('passwordConfirm').addEventListener('blur', function () {
 
     if (!this.value) {
-        fail('passwordConfirm', '비밀번호 재확인을 입력해 주세요.');
+        failTag('passwordConfirm', '비밀번호 재확인을 입력해 주세요.');
     }
 });
 
@@ -105,13 +118,13 @@ document.getElementById('passwordConfirm').addEventListener('blur', function () 
 document.getElementById('name').addEventListener('blur', function () {
 
     if  (!this.value) {
-        fail('name' ,'이름을 입력해 주세요.');
+        failTag('name' ,'이름을 입력해 주세요.');
 
     } else if (!namePattern.test(this.value)) {
-        fail('name', '한글, 영문 대/소문자를 사용해 주세요.');
+        failTag('name', '한글, 영문 대/소문자를 사용해 주세요.');
 
     } else {
-        success('name');
+        successTag('name');
     }
 });
 
@@ -123,10 +136,10 @@ document.getElementById('birth').addEventListener('blur', function () {
     const numberValue = birthValue.replace(/\D/g, '');
 
     if (!birthValue) {
-        fail('birth', '생년월일을 입력해 주세요.');
+        failTag('birth', '생년월일을 입력해 주세요.');
 
     } else if (!birthAndTelPattern.test(birthValue) || numberValue.length != 8) {
-        fail('birth', '생년월일 8자리 숫자로 입력해 주세요.');
+        failTag('birth', '생년월일 8자리 숫자로 입력해 주세요.');
 
     } else {
         let year = numberValue.slice(0, 4),
@@ -137,10 +150,10 @@ document.getElementById('birth').addEventListener('blur', function () {
         
         if (datePattern.test(date + '-' + month + '-' + year) && year >= currentYear - 100 && year <= currentYear - 7) {
             this.value = year + '-' + month + '-' + date;
-            success('birth');
+            successTag('birth');
 
         } else {
-            fail('birth', '생년월일이 정확한지 확인해 주세요.');
+            failTag('birth', '생년월일이 정확한지 확인해 주세요.');
         }
     }
 });
@@ -153,10 +166,10 @@ document.getElementById('phoneNo').addEventListener('blur', function () {
     const numberValue = phoneNoValue.replace(/\D/g, '');
 
     if (!phoneNoValue) {
-        fail('phoneNo', '휴대전화번호를 입력해 주세요.');
+        failTag('phoneNo', '휴대전화번호를 입력해 주세요.');
 
     } else if (!birthAndTelPattern.test(phoneNoValue) || numberValue.length != 11) {
-        fail('phoneNo', '휴대전화번호 11자리 숫자로 입력해 주세요.');
+        failTag('phoneNo', '휴대전화번호 11자리 숫자로 입력해 주세요.');
 
     } else {
         let firstNo = numberValue.slice(0, 3),
@@ -168,10 +181,10 @@ document.getElementById('phoneNo').addEventListener('blur', function () {
         if (phoneNoRegex.test(firstNo)) {
 
             this.value = firstNo + '-' + middleNo +'-'+ lastNo;
-            success('phoneNo');
+            successTag('phoneNo');
 
         } else {
-            fail('phoneNo', '휴대전화번호가 정확한지 확인해 주세요.');
+            failTag('phoneNo', '휴대전화번호가 정확한지 확인해 주세요.');
         }
     }
 });
@@ -198,27 +211,27 @@ function validateForm() {
 
         } else {
             if (!id[0].value) {
-                fail('id', '이메일을 입력해 주세요.');
+                failTag('id', '이메일을 입력해 주세요.');
             } else if (id[1].style.display === 'block') {
                 id[0].focus();
             } else if (!password[0].value) {
-                fail('password', '비밀번호를 입력해 주세요.');
+                failTag('password', '비밀번호를 입력해 주세요.');
             } else if (password[1].style.display === 'block') {
                 password[0].focus();
             } else if (!passwordConfirm[0].value) {
-                fail('passwordConfirm', '비밀번호 재확인을 입력해 주세요.');
+                failTag('passwordConfirm', '비밀번호 재확인을 입력해 주세요.');
             } else if (passwordConfirm[1].style.display === 'block') {
                 passwordConfirm[0].focus();
             } else if (!name[0].value) {
-                fail('name', '이름을 입력해 주세요.');
+                failTag('name', '이름을 입력해 주세요.');
             } else if (name[1].style.display === 'block') {
                 name[0].focus();
             } else if (!birth[0].value) {
-                fail('birth', '생년월일을 입력해 주세요.');
+                failTag('birth', '생년월일을 입력해 주세요.');
             } else if (birth[1].style.display === 'block') {
                 birth[0].focus();
             } else if (!phoneNo[0].value) {
-                fail('phoneNo', '휴대전화번호를 입력해 주세요.');
+                failTag('phoneNo', '휴대전화번호를 입력해 주세요.');
             } else if (phoneNo[1].style.display === 'block') {
                 phoneNo[0].focus();
             }
