1e66e7ad69131eada652d76e6244a3933148dbc9
zmgu <manggu94@gmail.com>
2023-12-06 23:59:19 +0900

    Autowired

--- src/main/java/com/okhwa/okdol/member/MemberController.java
+++ src/main/java/com/okhwa/okdol/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.okhwa.okdol.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/members")
 class MemberController {
-
+    @Autowired
     private MemberRepository repository;
 
     @GetMapping("/checkEmail/{email}")  // 이메일 중복 체크
