252e3079dba1952e00fe583e97d3c1be2d13c897
zmgu <manggu94@gmail.com>
2023-12-08 05:16:38 +0900

    Controller 수정

--- src/main/java/com/okhwa/okdol/member/MemberController.java
+++ src/main/java/com/okhwa/okdol/member/MemberController.java
@@ -1,25 +1,39 @@
 package com.okhwa.okdol.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/api/members")
+import java.time.LocalDate;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 class MemberController {
-    @Autowired
-    private MemberRepository repository;
+    private final MemberService memberService;
 
-    @GetMapping("/checkEmail/{email}")  // 이메일 중복 체크
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/{email}")  // 이메일 중복 체크
     public ResponseEntity<String> checkEmailExists(@PathVariable String email) {
-        boolean emailExists = repository.existsByEmail(email);
+        boolean emailExists = memberService.checkEmailExists(email);
         if (emailExists) {
             return ResponseEntity.ok("true");
         } else {
             return ResponseEntity.ok("false");
         }
     }
+
+    @RequestMapping(value="/members", method=RequestMethod.POST)
+    Member registerMember(Member member) {
+        if (member.getEmail() == null) {
+        // 필수 필드가 비어 있을 경우 예외 처리 또는 기본값 설정
+        throw new IllegalArgumentException("예외처리중");
+    }
+        System.out.println("member = " + member);
+        return memberService.save(member);
+    }
+
 }
+
