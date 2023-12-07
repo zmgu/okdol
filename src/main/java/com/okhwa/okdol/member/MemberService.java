366b74b06b5b15c290c2a2d1be14c561b52115bc
zmgu <manggu94@gmail.com>
2023-12-08 05:18:12 +0900

    Service 수정

--- src/main/java/com/okhwa/okdol/member/MemberService.java
+++ src/main/java/com/okhwa/okdol/member/MemberService.java
@@ -1,7 +1,20 @@
 package com.okhwa.okdol.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+
+    @Autowired
+    private MemberRepository repository;
+
+    public Boolean checkEmailExists(String email) {
+        return repository.existsByEmail(email);
+    }
+
+    Member save(Member member) {
+
+        return repository.save(member);
+    }
 }
