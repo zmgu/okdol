64b98abd8134e4b366a091cc993dda459940e2ba
zmgu <manggu94@gmail.com>
2023-12-12 19:56:37 +0900

    UserService 수정

--- src/main/java/com/okhwa/okdol/user/UserService.java
+++ src/main/java/com/okhwa/okdol/user/UserService.java
@@ -1,20 +1,29 @@
 package com.okhwa.okdol.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Boolean checkEmailExists(String email) {
-        return repository.existsByEmail(email);
+        return userRepository.existsByEmail(email);
     }
 
     User save(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
 
-        return repository.save(user);
+        user.setRole(Role.ROLE_USER);
+        user.setEnabled(true);
+
+        return userRepository.save(user);
     }
 }
