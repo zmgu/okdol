3864d1fea0b310e2eb9dbd0dad44c680a3797d2f
zmgu <manggu94@gmail.com>
2023-12-23 00:52:17 +0900

    RequiredArgsConstructor

--- src/main/java/com/okhwa/okdol/user/UserController.java
+++ src/main/java/com/okhwa/okdol/user/UserController.java
@@ -1,16 +1,14 @@
 package com.okhwa.okdol.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/users/{email}")  // 이메일 중복 체크
     public ResponseEntity<String> checkEmailExists(@PathVariable String email) {
